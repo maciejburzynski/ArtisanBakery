958c64d3ad1d9c176ff4e22a6cf152bc1f90b384
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-06 19:50:01 +0100

    Init Rest BreadController

--- bakery/src/main/java/pl/maciejburzynski/bakery/controller/BreadController.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/controller/BreadController.java
@@ -1,18 +1,30 @@
 package pl.maciejburzynski.bakery.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.maciejburzynski.bakery.entity.Bread;
+import pl.maciejburzynski.bakery.service.BreadService;
+
+import java.util.Set;
 
 @RestController
-@RequestMapping(value = ("/restapi/bread"))
+@RequiredArgsConstructor
+@RequestMapping(value = ("/restapi/breads"))
 public class BreadController {
 
+    private final BreadService breadService;
 
     @GetMapping
-    public String starter(){
-        return "Pierwszy rest";
+    public Set getBreads(){
+        return breadService.getAll();
     }
 
+    @PostMapping
+    public void addBread(Bread bread){
+        breadService.addBread(bread);
+    }
 
 }
