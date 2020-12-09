63d71d3979ccfae5507c5cdd70bc9d986636d283
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-09 19:43:02 +0100

    Swap Bread to JPA entity

--- bakery/src/main/java/pl/maciejburzynski/bakery/entity/Bread.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/entity/Bread.java
@@ -1,20 +1,29 @@
 package pl.maciejburzynski.bakery.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Bread {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
 
+    public Bread(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
 }
