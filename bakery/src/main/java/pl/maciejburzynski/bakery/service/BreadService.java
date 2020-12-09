7b0081b17d20f9b0ccbb31c383a0fd32f12fa39a
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-09 20:10:39 +0100

    Add update and delete bread method

--- bakery/src/main/java/pl/maciejburzynski/bakery/service/BreadService.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/service/BreadService.java
@@ -1,28 +1,36 @@
 package pl.maciejburzynski.bakery.service;
 
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.maciejburzynski.bakery.entity.Bread;
-import pl.maciejburzynski.bakery.repository.BreadRepository;
+import pl.maciejburzynski.bakery.repository.BreadCrudRepository;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class BreadService {
 
-    private final BreadRepository breadRepository;
+    private final BreadCrudRepository breadRepository;
 
 
     public Set<Bread> getAll() {
-    return breadRepository.getAll();
+        return breadRepository.getAll();
     }
 
     public void addBread(Bread bread) {
         breadRepository.addBread(bread);
     }
 
-    public void addBreads(Bread... breads) {
-        breadRepository.addBreads(breads);
+    public void deleteBreadById(Long id) {
+        breadRepository.deleteBreadById(id);
+    }
+
+    public void updateBread(Long id, String name, BigDecimal price) {
+        breadRepository.updateBread(id, name, price);
+
     }
 }
