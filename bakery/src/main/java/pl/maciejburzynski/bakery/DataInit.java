c2a4d427fe819a6c19c0877e9c4050d7c1465cf3
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-09 19:45:36 +0100

    Refactor of initial data

--- bakery/src/main/java/pl/maciejburzynski/bakery/DataInit.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/DataInit.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import pl.maciejburzynski.bakery.entity.Bread;
 import pl.maciejburzynski.bakery.service.BreadService;
 import java.math.BigDecimal;
@@ -23,8 +24,9 @@ public class DataInit {
     }
 
     private void addBreads() {
-        Bread bread = new Bread(1L,"Wielozbożowy", new BigDecimal(2.99));
-        Bread bread1 = new Bread(2L,"Pytlowy", new BigDecimal(3.99));
-        breadService.addBreads(bread, bread1);
+        Bread bread = new Bread("Wielozbożowy", BigDecimal.valueOf(2.99));
+        Bread bread1 = new Bread("Pytlowy", BigDecimal.valueOf(3.99));
+        breadService.addBread(bread);
+        breadService.addBread(bread1);
     }
 }
