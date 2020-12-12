b9f37b6bfaf59e681914c7c73d650c386d0586c0
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-12 17:25:46 +0100

    Add getBreadById() method in RestController

--- bakery/src/main/java/pl/maciejburzynski/bakery/service/BreadService.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/service/BreadService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.maciejburzynski.bakery.entity.Bread;
+import pl.maciejburzynski.bakery.exceptions.BreadNotFoundException;
 import pl.maciejburzynski.bakery.repository.BreadCrudRepository;
 
 import java.math.BigDecimal;
@@ -17,8 +18,13 @@ public class BreadService {
     private final BreadCrudRepository breadRepository;
 
 
-    public Set<Bread> getAll() {
+    public Set<Bread> getBreads() {
         return breadRepository.getAll();
+    }
+
+    public Bread getBreadById(Long id) {
+        return breadRepository.getBreadById(id)
+                .orElseThrow(() -> new BreadNotFoundException("Requested bread doesn't exist"));
     }
 
     public void addBread(Bread bread) {
@@ -31,6 +37,5 @@ public class BreadService {
 
     public void updateBread(Long id, String name, BigDecimal price) {
         breadRepository.updateBread(id, name, price);
-
     }
 }
