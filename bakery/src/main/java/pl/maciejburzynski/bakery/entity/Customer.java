d5f1f015831566bbaf3e656d98b9eb374344322f
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-13 23:28:38 +0100

    Add Customer Entity

---
+++ bakery/src/main/java/pl/maciejburzynski/bakery/entity/Customer.java
@@ -0,0 +1,27 @@
+package pl.maciejburzynski.bakery.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
