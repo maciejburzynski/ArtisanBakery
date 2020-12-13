725fcfb4745358421acc964679e3d037b302c5eb
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-13 23:30:56 +0100

    Add initial data for customer

--- bakery/src/main/java/pl/maciejburzynski/bakery/DataInit.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/DataInit.java
@@ -7,8 +7,14 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import pl.maciejburzynski.bakery.entity.Bread;
+import pl.maciejburzynski.bakery.entity.Customer;
 import pl.maciejburzynski.bakery.service.BreadService;
+import pl.maciejburzynski.bakery.service.CustomerService;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -16,17 +22,29 @@ import java.math.BigDecimal;
 public class DataInit {
 
   private final BreadService breadService;
+  private final CustomerService customerService;
 
 
     @EventListener(ApplicationReadyEvent.class)
     public void initBreads(){
+        addCustomers();
         addBreads();
     }
+
 
     private void addBreads() {
         Bread bread = new Bread("Wielozbożowy", BigDecimal.valueOf(2.99));
         Bread bread1 = new Bread("Pytlowy", BigDecimal.valueOf(3.99));
+
         breadService.addBread(bread);
         breadService.addBread(bread1);
+    }
+
+    private void addCustomers() {
+        Customer customer = new Customer("Andrzej","Andrzejewski");
+        Customer customer1 = new Customer("Marcin","Marcinowski");
+
+        customerService.addCustomer(customer);
+        customerService.addCustomer(customer1);
     }
 }
