81c032e2e6d5c9f25759a8c69de2b832009b0855
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-13 23:36:55 +0100

    Add service for cuentity

---
+++ bakery/src/main/java/pl/maciejburzynski/bakery/service/CustomerService.java
@@ -0,0 +1,33 @@
+package pl.maciejburzynski.bakery.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.maciejburzynski.bakery.entity.Customer;
+import pl.maciejburzynski.bakery.exceptions.CustomerNotFoundException;
+import pl.maciejburzynski.bakery.repository.CustomerCrudRepository;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerCrudRepository customerCrudRepository;
+
+    public void addCustomer(Customer customer) {
+        customerCrudRepository.addCustomer(customer);
+    }
+
+    public Set<Customer> getCustomers() {
+        return customerCrudRepository.getAll();
+    }
+
+    public Customer getCustomerById(Long id) {
+       return customerCrudRepository.getCustomerById(id)
+               .orElseThrow(() -> new CustomerNotFoundException("Customer with provided ID doesn't exist"));
+    }
+
+    public void deleteCustomerById(Long id) {
+        customerCrudRepository.deleteCustomerById(id);
+    }
+}
