ebc503f3abc52e79f0a5f2b7824d88f0d23f754f
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-12-21 16:52:27 +0100

    Add user entity

---
+++ bakery/src/main/java/pl/maciejburzynski/bakery/entity/User.java
@@ -0,0 +1,51 @@
+package pl.maciejburzynski.bakery.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends BasicEntity implements UserDetails {
+
+    private String username;
+    private String password;
+    private String mail;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
