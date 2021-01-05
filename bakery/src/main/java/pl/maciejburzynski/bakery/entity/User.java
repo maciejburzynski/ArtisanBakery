3904850e49c6bdad11a900d6615185d4be6da57e
Maciej Burzyński <maciej.kollaps@gmail.com>
2021-01-05 11:05:20 +0100

    Updated User entity including Spring Security adaptation

--- bakery/src/main/java/pl/maciejburzynski/bakery/entity/User.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/entity/User.java
@@ -1,15 +1,18 @@
 package pl.maciejburzynski.bakery.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import pl.maciejburzynski.bakery.security.UserRole;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -22,10 +25,20 @@ public class User extends BasicEntity implements UserDetails {
     private String password;
     private String mail;
     private String role;
+//    @Transient
+//    private Set <? extends GrantedAuthority> authorities;
+
+//    public User(Set<? extends GrantedAuthority> authorities) {
+//        this.authorities = authorities;
+//    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority(role));
+        if (role.equals("ADMIN")) {
+            return UserRole.ADMIN.getGrantedAuthorities();
+        } else {
+            return UserRole.USER.getGrantedAuthorities();
+        }
     }
 
     @Override
