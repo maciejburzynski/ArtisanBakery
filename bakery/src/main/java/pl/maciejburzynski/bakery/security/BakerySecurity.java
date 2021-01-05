a40ab68da1a1fc2165cff188b01d1fb07be2775f
Maciej Burzyński <maciej.kollaps@gmail.com>
2021-01-05 11:09:32 +0100

    Override security for login and logout

--- bakery/src/main/java/pl/maciejburzynski/bakery/security/BakerySecurity.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/security/BakerySecurity.java
@@ -3,6 +3,8 @@ package pl.maciejburzynski.bakery.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,28 +13,54 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.maciejburzynski.bakery.service.UserService;
 
+import static pl.maciejburzynski.bakery.security.UserPermission.*;
+import static pl.maciejburzynski.bakery.security.UserRole.ADMIN;
+
 @Configuration
 @RequiredArgsConstructor
 public class BakerySecurity extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userService;
+    private final PasswordEncoder encoder;
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().disable();
-//        http.authorizeRequests()
-//                .antMatchers("/api/breads").permitAll()
-//                .antMatchers("/signup").permitAll()
-//                .antMatchers("/register").permitAll()
-//                .antMatchers("/environment").hasAuthority("ROLE_ADMIN")
-//                .and()
-//                .formLogin();
+        http
+                .csrf().disable()
+                .headers().disable()
+                .authorizeRequests()
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/environment").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/orders/**").hasAuthority(ORDERS_READ.name())
+                .antMatchers(HttpMethod.POST,"/api/orders/**").hasAuthority(ORDERS_ADD.name())
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/signup")
+                    .permitAll()
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .logoutSuccessUrl("/")
+                    .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(userService);
+        return provider;
     }
 }
