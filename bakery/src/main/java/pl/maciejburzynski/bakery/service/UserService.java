4aceb41d83f6b419137b95e597af543f2297d65f
Maciej Burzyński <maciej.kollaps@gmail.com>
2021-01-05 11:15:27 +0100

    Separate adding user within initialization and via registration form

--- bakery/src/main/java/pl/maciejburzynski/bakery/service/UserService.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/service/UserService.java
@@ -9,6 +9,11 @@ import pl.maciejburzynski.bakery.entity.User;
 import pl.maciejburzynski.bakery.exceptions.UserNotFoundException;
 import pl.maciejburzynski.bakery.repository.UserCrudRepository;
 import pl.maciejburzynski.bakery.security.BakerySecurity;
+import pl.maciejburzynski.bakery.security.UserRole;
+
+import javax.mail.MessagingException;
+
+import static pl.maciejburzynski.bakery.security.UserRole.USER;
 
 @Service
 @RequiredArgsConstructor
@@ -16,16 +21,26 @@ public class UserService {
 
     private final UserCrudRepository userCrudRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
 
 
     public UserDetails findByUsername(String s) {
         return userCrudRepository.findByUsername(s)
-                .orElseThrow(() -> new UserNotFoundException("User doesn't exist"));
+                .orElseThrow(() -> new UserNotFoundException("User doesn't exist " + s));
     }
 
-    public void addUser(User user) {
-        user.setRole("ROLE_USER");
+    public void addUser(User user) throws MessagingException {
+//        user.setGrantedAuthorities(USER.getGrantedAuthorities());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+            mailService.sendMail(user.getMail(),
+                    "Witaj Piekarzu!",
+                    "Jeśli dostałeś tego maila - jest dobrze ;)",
+                    false);
+
+        userCrudRepository.addUser(user);
+    }
+
+    public void addInitialUser(User user) {
         userCrudRepository.addUser(user);
     }
 }
