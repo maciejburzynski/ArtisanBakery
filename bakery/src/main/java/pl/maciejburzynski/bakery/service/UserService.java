bd2ffb816eb5f6b260969fd2f3bfffdb28249124
Maciej Burzyński <maciej.kollaps@gmail.com>
2021-01-16 17:34:30 +0100

    Add functionality of acitivating token

--- bakery/src/main/java/pl/maciejburzynski/bakery/service/UserService.java
+++ bakery/src/main/java/pl/maciejburzynski/bakery/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.maciejburzynski.bakery.BakeryConfig;
+import pl.maciejburzynski.bakery.entity.Token;
 import pl.maciejburzynski.bakery.entity.User;
 import pl.maciejburzynski.bakery.exceptions.UserNotFoundException;
 import pl.maciejburzynski.bakery.repository.UserCrudRepository;
@@ -22,26 +23,46 @@ public class UserService {
     private final UserCrudRepository userCrudRepository;
     private final PasswordEncoder passwordEncoder;
     private final MailService mailService;
+    private final TokenService tokenService;
 
+
+    public void saveUser(User user) throws MessagingException {
+        userCrudRepository.addUser(user);
+    }
 
     public UserDetails findByUsername(String s) {
         return userCrudRepository.findByUsername(s)
                 .orElseThrow(() -> new UserNotFoundException("User doesn't exist " + s));
     }
 
-    public void addUser(User user) throws MessagingException {
-        user.setRole(USER.name());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-            mailService.sendMail(user.getMail(),
-                    "Witaj Piekarzu!",
-                    "Jeśli dostałeś tego maila - jest dobrze ;)",
-                    false);
-
-        userCrudRepository.addUser(user);
-    }
 
     public void addInitialUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+    }
+
+    public void activateUser(User user) {
+        user.setRole(USER.name());
+        user.setEnable(true);
         userCrudRepository.addUser(user);
+    }
+
+    public void sendTokenToUser(User user) throws MessagingException {
+
+        Token token = new Token();
+        token.setValue(tokenService.generateToken());
+        token.setUser(user);
+        tokenService.saveToken(token);
+
+        mailService.sendMail(user.getMail(),
+                "Witaj Piekarzu!",
+                "Jeśli dostałeś tego maila - jest dobrze ;) Twój kod aktywacyjny to "
+                        + token.getValue(),
+                false);
+
+    }
+
+    public void addInactiveUserToDatabase(User userToBeRegisteredHolder) {
+        userToBeRegisteredHolder.setEnable(false);
+        userToBeRegisteredHolder.setRole(USER.name());
     }
 }
